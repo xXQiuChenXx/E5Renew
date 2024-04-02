c3ab508296dd819551e465a3fc08e7615c7ae320
xXQiuChenXx <lautaihong2004@gmail.com>
2024-04-03 00:00:17 +0800

    decorate

--- GraphAPI/index.ts
+++ GraphAPI/index.ts
@@ -1,21 +1,20 @@
 import { Client, ClientOptions } from "@microsoft/microsoft-graph-client";
-import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
-import { DeviceCodeInfo, DeviceCodeCredential } from "@azure/identity";
 import { User, Message } from "@microsoft/microsoft-graph-types";
 import { settings } from "types";
-import { existsSync, readFileSync, readdirSync, writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFileSync } from "fs";
 import { GraphAPI } from "./utils";
 import msal from "@azure/msal-node";
 
 class GraphAPIClient {
-  #settings: settings;
-  #client: Client;
+  private settings: settings;
+  private client: Client;
+  
   constructor(settings: settings) {
-    this.#settings = settings;
+    this.settings = settings;
   }
 
   async login() {
-    const settings = this.#settings;
+    const settings = this.settings;
     const testClient = new msal.PublicClientApplication({
       auth: {
         clientId: settings.clientId,
@@ -24,6 +23,7 @@ class GraphAPIClient {
         cachePlugin: {
           async beforeCacheAccess(tokenCacheContext) {
             if (existsSync("./cache.json")) {
+              console.log("ok")
               const cache = await readFileSync("./cache.json", "utf-8");
               await tokenCacheContext.tokenCache.deserialize(cache);
             }
@@ -45,19 +45,19 @@ class GraphAPIClient {
 
     if(!res?.accessToken) return;
 
-    this.#client = Client.init({
+    this.client = Client.init({
       authProvider: (done) => {
         done(null, res.accessToken)
       }
     })
 
-    const graphAPI = new GraphAPI(this.#client);
+    const graphAPI = new GraphAPI(this.client);
     const response = await graphAPI._getInbox();
     console.log(response);
   }
 
   _getClient() {
-    return this.#client;
+    return this.client;
   }
 }
 
