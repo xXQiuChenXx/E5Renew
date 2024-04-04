8d84cae339ce5f3cd9c3a1296e0c967fd4d12cd6
xXQiuChenXx <lautaihong2004@gmail.com>
2024-04-04 22:35:44 +0800

    cache implementation and debug

--- GraphAPI/index.ts
+++ GraphAPI/index.ts
@@ -3,53 +3,65 @@ import { User, Message } from "@microsoft/microsoft-graph-types";
 import { settings } from "types";
 import { existsSync, readFileSync, writeFileSync } from "fs";
 import { GraphAPI } from "./utils";
-import msal from "@azure/msal-node";
+import { PublicClientApplication } from "@azure/msal-node";
 
 class GraphAPIClient {
   private settings: settings;
   private client: Client;
-  
+
   constructor(settings: settings) {
     this.settings = settings;
   }
 
   async login() {
     const settings = this.settings;
-    const testClient = new msal.PublicClientApplication({
+
+    const testClient = new PublicClientApplication({
       auth: {
         clientId: settings.clientId,
       },
       cache: {
         cachePlugin: {
-          async beforeCacheAccess(tokenCacheContext) {
-            if (existsSync("./cache.json")) {
-              console.log("ok")
-              const cache = await readFileSync("./cache.json", "utf-8");
-              await tokenCacheContext.tokenCache.deserialize(cache);
-            }
-          },
           async afterCacheAccess(tokenCacheContext) {
             const cache = await tokenCacheContext.tokenCache.serialize();
-            await writeFileSync("./cache.json", cache, "utf-8");
+            writeFileSync("./cache.json", JSON.stringify(JSON.parse(cache), null, 4));
+          },
+          async beforeCacheAccess(tokenCacheContext) {
+            if (existsSync("./cache.json")) {
+              const content = readFileSync("./cache.json", "utf-8");
+              await tokenCacheContext.tokenCache.deserialize(content);
+            }
           },
         },
       },
     });
 
-    const res = await testClient.acquireTokenByDeviceCode({
-      scopes: settings.graphUserScopes,
-      deviceCodeCallback: (response) => {
-        console.log(response);
-      },
-    });
+    let res: any;
 
-    if(!res?.accessToken) return;
+    if (existsSync("./cache.json")) {
+      const acc = await testClient.getTokenCache().getAllAccounts();
+      res = await testClient.acquireTokenSilent({
+        scopes: settings.graphUserScopes,
+        account: acc[0],
+      });
+    } else {
+      res = await testClient.acquireTokenByDeviceCode({
+        scopes: settings.graphUserScopes,
+        deviceCodeCallback: (response) => {
+          console.log(response);
+        },
+      });
+    }
+
+    console.log(res);
+
+    if (!res?.accessToken) return;
 
     this.client = Client.init({
       authProvider: (done) => {
-        done(null, res.accessToken)
-      }
-    })
+        done(null, res.accessToken);
+      },
+    });
 
     const graphAPI = new GraphAPI(this.client);
     const response = await graphAPI._getInbox();
